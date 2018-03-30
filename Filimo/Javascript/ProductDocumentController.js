ca7287d3e101636d30c3f80e97fc3bfc3cf495e4
Saeed Taheri <saeed.taheri@gmail.com>
2018-03-31 00:20:00 +0430

    Showing crew members and actors' names in product detail.

--- Filimo/Javascript/ProductDocumentController.js
+++ Filimo/Javascript/ProductDocumentController.js
@@ -14,6 +14,7 @@ class ProductDocumentController extends DocumentController {
         const playButton = document.getElementById("playButton")
         const bookmarkButton = document.getElementById('bookmarkButton')
         const previewButton = document.getElementById('previewButton')
+        const castsShelf = document.getElementById('castsShelf')
 
         previewButton.parentNode.removeChild(previewButton)
 
@@ -49,10 +50,23 @@ class ProductDocumentController extends DocumentController {
             }
         })    
 
+        let recommendationSectionNode = document.getElementById("recommendation")
+        let recommendationURL = filimoAPIBaseURL + '/recom/uid/' + this._productInfo.uid
+        this._dataLoader._fetchJSONData(this._documentLoader.prepareURL(recommendationURL), (dataObj) => {
+            let movies = dataObj.recom
+            recommendationSectionNode.dataItem = new DataItem()
+            recommendationSectionNode.dataItem.setPropertyPath("items", dataItemsFromJSONItems(movies))
+
+            document.getElementById("recommendationStaticTitle").textContent = "پیشنهادها"
+        })
+
         let detailInfoURL = filimoAPIBaseURL + '/moviedetail/uid/' + this._productInfo.uid
         this._dataLoader._fetchJSONData(this._documentLoader.prepareURL(detailInfoURL), (dataObj) => {
-            
-            if (dataObj.moviedetail && dataObj.moviedetail.trailer && dataObj.moviedetail.trailer.length > 0) {
+            if (dataObj.moviedetail === 'null' || dataObj.moviedetail === 'undefined') {
+                return
+            }
+
+            if (dataObj.moviedetail.trailer && dataObj.moviedetail.trailer.length > 0) {
                 playButton.parentNode.insertBefore(previewButton, playButton)
                 previewButton.addEventListener('select', (event) => {
                     playTrailer(dataObj.moviedetail.trailer[0])
@@ -60,6 +74,56 @@ class ProductDocumentController extends DocumentController {
                 previewButton.addEventListener('play', (event) => {
                     playTrailer(dataObj.moviedetail.trailer[0])
                 })
+            }
+            
+            if (dataObj.moviedetail.crew && dataObj.moviedetail.crew.length > 0) {
+                let castsSection = castsShelf.getElementsByTagName("section").item(0)
+                castsShelf.getElementsByTagName("title").item(0).textContent = 'عوامل'
+                createLockups(dataObj.moviedetail.crew, castsSection)
+            }
+
+            function createLockups(crew, castsSection) {
+                let directorNode = document.getElementById("directorInfo")
+                let actors = []
+                for(let i = 0; i < crew.length; i++) {
+                    let item = crew[i]
+                    item.profile.forEach((profile) => {
+                        let names = profile.name_fa.split(' ')
+                        let lockup = `<monogramLockup>
+                        <monogram firstName="${names[0]}" lastName="${names[names.length - 1]}" />
+                            <title>${profile.name_fa}</title>
+                            <subtitle>${item.post_info.title_fa}</subtitle>
+                        </monogramLockup>`
+                        
+                        if (item.post_info.title_fa === 'کارگردان' && directorNode.textContent === '') {
+                            directorNode.textContent = profile.name_fa
+                        }
+
+                        if (item.post_info.title_fa.includes('بازیگر')) {
+                            actors.push(profile.name_fa)
+                        }
+                        castsSection.insertAdjacentHTML('beforeend', lockup)
+                    })
+                }
+                if (directorNode.textContent === '') {
+                    directorNode.parentNode.parentNode.removeChild(directorNode.parentNode)
+                } else {
+                    directorNode.parentNode.getElementsByTagName('title').item(0).textContent = 'کارگردان'
+                }
+                if (actors.length > 0) {
+                    let infoNode = `<info>
+                    <header>
+                        <title>بازیگران</title>
+                    </header>`
+                    for (let i = 0; i < Math.min(3, actors.length); i++) {
+                        infoNode += `<text>${actors[i]}</text>
+                        `
+                    }
+                    infoNode += '</info>'
+
+                    let infoListNode = document.getElementsByTagName('infoList').item(0)
+                    infoListNode.insertAdjacentHTML('beforeend', infoNode)
+                }
             }
         })
 
@@ -115,23 +179,6 @@ class ProductDocumentController extends DocumentController {
             let genreToAdd = `<text>${this._productInfo.category_2}</text>`
             document.getElementById("genreInfo").insertAdjacentHTML('beforeend', genreToAdd)
         }
-
-        let directorNode = document.getElementById("directorInfo")
-        if (this._productInfo.director_fa == null) {
-            directorNode.parentNode.parentNode.removeChild(directorNode.parentNode)
-        } else {
-            directorNode.textContent = this._productInfo.director_fa 
-        }
-
-        let recommendationSectionNode = document.getElementById("recommendation")
-        let recommendationURL = filimoAPIBaseURL + '/recom/uid/' + this._productInfo.uid
-        this._dataLoader._fetchJSONData(this._documentLoader.prepareURL(recommendationURL), (dataObj) => {
-            let movies = dataObj.recom
-            recommendationSectionNode.dataItem = new DataItem()
-            recommendationSectionNode.dataItem.setPropertyPath("items", dataItemsFromJSONItems(movies))
-
-            document.getElementById("recommendationStaticTitle").textContent = "پیشنهادها"
-        })
 
         function setupBookmarkButton(movieMoreInfo) {
             if (movieMoreInfo.has_wish) {
