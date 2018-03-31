5fa9218e39abbb68dbfcc09c0117c2f48d3222b9
Saeed Taheri <saeed.taheri@gmail.com>
2018-03-31 19:14:12 +0430

    Adds a target for Top Shelf support

---
+++ TopShelf/ServiceProvider.swift
@@ -0,0 +1,31 @@
+//
+//  ServiceProvider.swift
+//  TopShelf
+//
+//  Created by Saeed Taheri on 3/31/18.
+//  Copyright © 2018 Filimo. All rights reserved.
+//
+
+import Foundation
+import TVServices
+
+class ServiceProvider: NSObject, TVTopShelfProvider {
+
+    override init() {
+        super.init()
+    }
+
+    // MARK: - TVTopShelfProvider protocol
+
+    var topShelfStyle: TVTopShelfContentStyle {
+        // Return desired Top Shelf style.
+        return .sectioned
+    }
+
+    var topShelfItems: [TVContentItem] {
+        // Create an array of TVContentItems.
+        return []
+    }
+
+}
+
