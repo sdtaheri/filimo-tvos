d0ae59ad02dc3bd0828db8745d907c98846cb67f
Saeed Taheri <saeed.taheri@gmail.com>
2018-04-14 21:08:47 +0430

    More movies are shown in ProductsList page as the user scrolls down.

--- Main/Javascript/ProductsListDocumentController.js
+++ Main/Javascript/ProductsListDocumentController.js
@@ -7,36 +7,75 @@ class ProductsListDocumentController extends DocumentController {
     setupDocument(document) {
         super.setupDocument(document)
 
+        const stackTemplate = document.getElementsByTagName("stackTemplate").item(0)
         const banner = document.getElementsByTagName("banner").item(0)
+        const grid = document.getElementsByTagName("grid").item(0)
+        
+        const requestType = this._sourceDataItem.requestType
+        let dataLoadingURL = null
 
-        switch (this._sourceDataItem.requestType) {
+        const dataLoader = this._dataLoader
+        const documentLoader = this._documentLoader
+
+        switch (requestType) {
             case 'search':
                 banner.getElementsByTagName("title").item(0).textContent = this._sourceDataItem.queryString
                 
-                let searchURL = filimoAPIBaseURL + '/search/text/' + encodeURIComponent(this._sourceDataItem.queryString) + '/perpage/50' 
-                this._dataLoader._fetchJSONData(this._documentLoader.prepareURL(searchURL), (dataObj) => {
-                    fillGrid(dataObj.search)
+                dataLoadingURL = filimoAPIBaseURL + '/search/text/' + encodeURIComponent(this._sourceDataItem.queryString) + '/perpage/20' 
+                dataLoader._fetchJSONData(documentLoader.prepareURL(dataLoadingURL), (dataObj) => {
+                    fillGrid(dataObj)
                 })
 
                 break
             case 'category':
                 banner.getElementsByTagName("title").item(0).textContent = this._sourceDataItem.title
 
-                let url = filimoAPIBaseURL + '/movielistbycat/catid/' + this._sourceDataItem.id + '/perpage/50/'
-                this._dataLoader._fetchJSONData(this._documentLoader.prepareURL(url), (dataObj) => {
-                    fillGrid(dataObj.movielistbycat)
+                dataLoadingURL = filimoAPIBaseURL + '/movielistbycat/catid/' + this._sourceDataItem.id + '/perpage/20/'
+                dataLoader._fetchJSONData(documentLoader.prepareURL(dataLoadingURL), (dataObj) => {
+                    fillGrid(dataObj)
                 })
             
                 break
             default: return
         }
 
-        function fillGrid(movies) {
+        stackTemplate.addEventListener('needsmore', (event) => {
+            if (dataLoadingURL == null) {
+                return
+            }
+            dataLoader._fetchJSONData(documentLoader.prepareURL(dataLoadingURL), (dataObj) => {
+                fillGrid(dataObj)
+            })
+        })
+
+        function fillGrid(dataObj) {
+            let movies = null
+
+            switch (requestType) {
+                case 'search':
+                    movies = dataObj.search
+                    break
+                case 'category':
+                    movies = dataObj.movielistbycat
+                    break
+                default: return
+            }
+
+            if (dataObj.ui.pagingForward && dataObj.ui.pagingForward.length > 0) {
+                dataLoadingURL = dataObj.ui.pagingForward
+            } else {
+                dataLoadingURL = null
+            }
+
+            let section = grid.getElementsByTagName("section").item(0)    
             if (movies) {
-                let grid = document.getElementsByTagName("grid").item(0)
-                let section = grid.getElementsByTagName("section").item(0)    
-                section.dataItem = new DataItem()
-                section.dataItem.setPropertyPath("items", dataItemsFromJSONItems(movies))
+                if (section.dataItem) {
+                    Array.prototype.push.apply(section.dataItem.items, dataItemsFromJSONItems(movies))
+                    section.dataItem.touchPropertyPath("items")
+                } else {
+                    section.dataItem = new DataItem()
+                    section.dataItem.setPropertyPath("items", dataItemsFromJSONItems(movies))
+                }
             }
         }
 
