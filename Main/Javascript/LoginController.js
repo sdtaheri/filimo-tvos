c2804d938a5aa00e68a4eda9b88d07ad9def8f63
Saeed Taheri <saeed.taheri@gmail.com>
2022-05-08 21:45:48 +0430

    Show login instructions in a clearer manner.
    Reindent codes in LoginController, Strings.js and Login.xml.

--- Main/Javascript/LoginController.js
+++ Main/Javascript/LoginController.js
@@ -1,40 +1,42 @@
 class LoginController extends DocumentController {
 
+  setupDocument (document) {
+    super.setupDocument(document)
 
-    setupDocument(document) {
-        super.setupDocument(document)
+    const description1 = document.getElementById('descContainer1')
+    const description2 = document.getElementById('descContainer2')
+    description1.textContent = string_login_description()
+    description2.textContent = string_login_description_use_qr_code
 
-        const description = document.getElementById("descTitle");
-        description.textContent = string_login_description();
+    this.dataLoader.fetchLoginCode((result) => {
+      const image = document.getElementById('codeImage')
+      const codeText = document.getElementById('verificationCode')
 
-        this.dataLoader.fetchLoginCode((result) => {
-            const image = document.getElementById("codeImage");
-            const codeText = document.getElementById("verificationCode");
+      codeText.textContent = result.code
+      image.setAttribute('src', result.qrImage)
 
-            codeText.textContent = result.code;
-            image.setAttribute("src", result.qrImage);
+      let refreshIntervalId = setInterval(() => {
+        this.dataLoader.verifyLogin(result.code, (verificationResult) => {
+          if (verificationResult.jwtToken !== null) {
+            clearInterval(refreshIntervalId)
+            UserManager.setJwtToken(verificationResult.jwtToken)
+            UserManager.setUsername(verificationResult.username)
+            UserManager.setLToken(verificationResult.lToken)
 
-            let refreshIntervalId = setInterval(() => {
-                this.dataLoader.verifyLogin(result.code, (verificationResult) => {
-                    if (verificationResult.jwtToken !== null) {
-                        clearInterval(refreshIntervalId);
-                        UserManager.setJwtToken(verificationResult.jwtToken);
-                        UserManager.setUsername(verificationResult.username);
-                        UserManager.setLToken(verificationResult.lToken);
+            navigationDocument.popDocument()
+          }
+        })
+      }, 5000)
+      this._refreshIntervalId = refreshIntervalId
+    })
+  }
 
-                        navigationDocument.popDocument();
-                    }
-                });
-            }, 5000);
-            this._refreshIntervalId = refreshIntervalId
-        });
+  handleEvent (event) {
+    if (event.type === 'unload') {
+      clearInterval(this._refreshIntervalId)
     }
-
-    handleEvent(event) {
-        if (event.type === "unload") {
-            clearInterval(this._refreshIntervalId)
-        }
-        super.handleEvent(event)
-    }
+    super.handleEvent(event)
+  }
 }
-registerAttributeName("loginDocumentURL", LoginController)
+
+registerAttributeName('loginDocumentURL', LoginController)
