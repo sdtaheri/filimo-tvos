004a6451aaf0bfdd88d3dbf6389ea10f7736a8ba
Saeed Taheri <saeed.taheri@gmail.com>
2022-05-15 16:09:51 +0430

    Clean up MenuBarController.

--- Main/Javascript/MenuBarController.js
+++ Main/Javascript/MenuBarController.js
@@ -6,71 +6,36 @@ This class handles presenting the Menu Bar template example.
 */
 
 class MenuBarController extends DocumentController {
+  setupDocument (document) {
+    const menuBarElem = document.getElementsByTagName('menuBar').item(0)
 
-    fetchDocument(documentURL, loadingDocument) {
-        this.documentLoader.fetch({
-            url: documentURL,
-            success: (menuBarDocument) => {
-                const menuBarElem = menuBarDocument.getElementsByTagName("menuBar").item(0);
-                menuBarElem.addEventListener("select", (event) => {
-                    this.selectMenuItem(event.target);
-                });
+    menuBarElem.addEventListener('select', (event) => {
+      this.selectMenuItem(event.target)
+    })
+  }
 
-                // Pre-load the document for the initial focused menu item or first item,
-                // before presenting the menuBarTemplate on navigation stack.
-                // const initialMenuItemElem = this.findInitialMenuItem(menuBarElem);
-                // this.selectMenuItem(initialMenuItemElem, true, () => {
-                this.handleDocument(menuBarDocument, loadingDocument);
-                // });
+  selectMenuItem (menuItemElem) {
+    const menuBarElem = menuItemElem.parentNode
+    const menuBarFeature = menuBarElem.getFeature('MenuBarDocument')
+    const existingDocument = menuBarFeature.getDocument(menuItemElem)
 
-            },
-            error: (xhr) => {
-                const alertDocument = createLoadErrorAlertDocument(documentURL, xhr, false);
-                this.handleDocument(alertDocument, loadingDocument);
-            }
-        });
-    }
-
-    findInitialMenuItem(menuBarElem) {
-        let highlightIndex = 0;
-        const menuItemElems = menuBarElem.childNodes;
-        for (let i = 0; i < menuItemElems.length; i++) {
-            if (menuItemElems.item(i).hasAttribute("autoHighlight")) {
-                highlightIndex = i;
-                break;
-            }
+    if (!existingDocument) {
+      const controllerOptions = resolveControllerFromElement(menuItemElem)
+      if (controllerOptions) {
+        menuBarFeature.setDocument(createLoadingDocument(), menuItemElem)
+        controllerOptions.documentLoader = this.documentLoader
+        controllerOptions.linkKey = menuItemElem.getAttribute('linkKey') ||
+          null
+        const controllerClass = controllerOptions.type
+        const controller = new controllerClass(controllerOptions)
+        controller.handleDocument = (document) => {
+          setTimeout(() => {
+            menuBarFeature.setDocument(document, menuItemElem)
+          }, 500)
         }
-        return menuItemElems.item(highlightIndex);
+      }
     }
-
-    selectMenuItem(menuItemElem, isInitialItem, doneCallback) {
-        const menuBarElem = menuItemElem.parentNode;
-        const menuBarFeature = menuBarElem.getFeature("MenuBarDocument");
-        const existingDocument = menuBarFeature.getDocument(menuItemElem);
-
-        if (!existingDocument) {
-            const controllerOptions = resolveControllerFromElement(menuItemElem);
-            if (controllerOptions) {
-                if (!isInitialItem) {
-                    menuBarFeature.setDocument(createLoadingDocument(), menuItemElem);
-                }
-                controllerOptions.documentLoader = this.documentLoader;
-                controllerOptions.linkKey = menuItemElem.getAttribute('linkKey') || null;
-                const controllerClass = controllerOptions.type;
-                const controller = new controllerClass(controllerOptions);
-                controller.handleDocument = (document) => {
-                    if (isInitialItem) {
-                        menuBarFeature.setDocument(document, menuItemElem);
-                    } else {
-                        setTimeout(function() {
-                            menuBarFeature.setDocument(document, menuItemElem);
-                        }, 500);
-                    }
-                    doneCallback && doneCallback();
-                };
-            }
-        }
-    }
+  }
 }
 
-registerAttributeName('menuBarDocumentURL', MenuBarController);
+registerAttributeName('menuBarDocumentURL', MenuBarController)
